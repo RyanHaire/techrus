01dbc362ca540fd01c1db664651e0c9c942f6f1a
Ryan Haire <rwhaire@hotmail.com>
2021-01-15 09:25:03 -0500

    fixed title

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,9 +17,6 @@ const Header = () => {
                             <LinkContainer to="/cart">
                                 <Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
                             </LinkContainer>
-                            <LinkContainer to="/login">
-                                <Nav.Link><i className="fas fa-user"></i> Sign In</Nav.Link>
-                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
