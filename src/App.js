b869fee2b1b3b637aee740c2196191cb5f5cfaa2
Ryan Haire <rwhaire@hotmail.com>
2021-01-16 08:38:07 -0500

    added 404 page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import NotFoundScreen from './screens/NotFoundScreen'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
             <Route path='/' component={HomeScreen} exact/>
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
+            <Route component={NotFoundScreen}/>
           </Container>
         </main>
         <Footer/>
